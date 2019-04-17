e2f2e78c5317fb62c3ad67d5fbcb0b7a7daa133a
thillabp <thillabp@gmail.com>
2019-04-17 14:00:13 -0300

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#slide img:eq(0)").addClass("ativo").show();
+    var texto = $(".ativo").attr("alt");
+    $("#slide").prepend("<p>" + texto + "</p>");
+    
+    setInterval(slide, 1000);
+    
+    function slide() {
+        if ($(".ativo").next().size()) {
+            $(".ativo").fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+        }else{
+        $(".ativo").fadeOut().removeClass("ativo");
+        $("#slide img:eq(0)").fadeIn().addClass("ativo");
+        }
+    
+    var texto = $(".ativo").attr("alt");
+    $("#slide p").hide().html(texto).delay(500).fadeIn();
+
+}});
+ 
+
